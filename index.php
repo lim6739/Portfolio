f8bddac2f3bf94cc91c5b2f8e24883098c6333fe
lim6739 <limmacren@gmail.com>
2025-08-17 15:07:58 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,215 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Portfolio</title>
+    <link rel="stylesheet" href="./assets/css/navbar.css">
+    <link rel="stylesheet" href="./assets/css/index.css">
+    <script src="https://kit.fontawesome.com/186f1479cd.js" crossorigin="anonymous"></script>
+    <script src="./assets/js/finisher-header.es5.min.js" type="text/javascript"></script>
+</head>
+<body>
+
+    <nav class="navbar">
+        <div class="navdiv">
+            <div class="title"><h1>My Portfolio</h1></div>
+            <div class="menu">
+                <ul class="menu-list">
+                    <li><a href="" class="active">Home</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#projects">Projects</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                    <div class="btn">
+                        <button><span>n mode</span></button>
+                    </div>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    
+    <div class="container">
+        <main>
+            <div class="header finisher-header" style="width: 100%;">
+            <div class="hero" id="home">
+                <div class="info">
+                    <div class="wrap">
+                        <div class="title-main">
+                            <h1>Hi! I'm Macren L. Lim</h1>
+                        </div>
+                        <div class="info1">
+                            <span>Passionate about web development, graphic design, and digital media. Dedicated to learning and creating meaningful projects.</span>
+                        </div>
+                        <div class="links">
+                            <div class="link">
+                                <a href="https://github.com/lim6739" target="_blank"><i class="fa-brands fa-github"></i></a>
+                            </div>
+                            <div class="link">
+                                <a href="https://www.facebook.com/macren.lim.3/" target="_blank"><i class="fa-brands fa-facebook"></i></a>
+                            </div>
+                            <div class="link">
+                                <a href="https://www.instagram.com/limmacren/" target="_blank"><i class="fa-brands fa-square-instagram"></i></a>
+                            </div>
+                            <div class="link">
+                                <a href="https://www.linkedin.com/in/limmacren" target="_blank"><i class="fa-brands fa-square-linkedin"></i></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="img">
+                        <img src="./assets/img/macren.png" alt="Profile Pic" width="350px">
+                    </div>
+                </div>
+            </div>
+            </div>
+        </main>
+        <div class="section">
+            <div class="section1" id="about">
+                <div class="skillsInfo2">
+                    <div>
+                        <img src="./assets/img/workspace.png" alt="workspace.png" width="350px">
+                    </div>
+                    <div class="aboutMe">
+                        <div class="about">
+                            <div class="title2">
+                                <h2>ABOUT ME</h2>
+                            </div>
+                            <span>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi delectus, quaerat reprehenderit temporibus velit magni nostrum, in aliquid fugiat atque ex iure laborum quos animi tempora fugit eos earum libero?</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="divskills">
+                    <div class="skill">
+                        <span class="tech"><h2>My Skills</h2></span>
+                    </div>
+                    <div class="skillsImg">
+                        <div class="skillsInfo">
+                            <div class="skillsImg1">
+                                <div class="logo">
+                                    <div class="logos">
+                                        <img src="./assets/img/html.png" alt="html.png">
+                                        <span>HTML</span>
+                                    </div>
+                                </div>
+                                <div class="logo">
+                                    <div class="logos">
+                                        <img src="./assets/img/css.png" alt="css.png">
+                                        <span>CSS</span>
+                                    </div>
+                                </div>
+                                <div class="logo">
+                                    <div class="logos">
+                                        <img src="./assets/img/js.png" alt="js.png">
+                                        <span>JavaScript</span>
+                                    </div>
+                                </div>
+                                <div class="logo">
+                                    <div class="logos">
+                                        <img src="./assets/img/php.png" alt="php.png">
+                                        <span>Php</span>
+                                    </div>
+                                </div>
+                                <div class="logo">
+                                    <div class="logos">
+                                        <img src="./assets/img/bootstrap.png" alt="bootstrap.png">
+                                        <span>Bootstrap</span>
+                                    </div>
+                                </div>
+                                <div class="logo">
+                                    <div class="logos">
+                                        <img src="./assets/img/figma.png" alt="figma.png">
+                                        <span>Figma</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="section2" id="projects">
+                <div class="titlecard">
+                    <h2>Projects</h2>
+                </div>
+                <div class="allCard">
+                    <div class="project-row">
+                        <div class="project-img">
+                            <img src="./assets/img/project1.png" alt="Project 1">
+                        </div>
+                        <div class="project-description">
+                            <h3>Project 1 Title</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis fugiat consequuntur doloremque laboriosam eum cum obcaecati maxime voluptatum exercitationem nobis accusamus dignissimos suscipit, voluptas earum doloribus! Aut voluptas molestiae quam!</p>
+                        </div>
+                    </div>
+                    <div class="project-row">
+                        <div class="project-description">
+                            <h3>Project 2 Title</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit consequatur saepe, nesciunt quibusdam qui ab? Beatae illum odit corrupti quas dolor dolorum reiciendis cum rem quod provident? Modi, ipsum dolores?</p>
+                        </div>
+                        <div class="project-img">
+                            <img src="./assets/img/project2.png" alt="Project 2">
+                        </div>
+                    </div>
+                    <div class="project-row">
+                        <div class="project-img">
+                            <img src="./assets/img/project2.png" alt="Project 2">
+                        </div>
+                        <div class="project-description">
+                            <h3>Project 2 Title</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam vitae quod repellat veniam est veritatis soluta voluptas quae velit error nisi, totam, accusamus ad fugit. Aliquam labore laudantium recusandae aliquid!</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="section3" id="contact">
+                
+            </div>
+        </div>
+        <footer>
+            <span>© 2025 Macren Lacsina Lim. All Rights Reserved.</span>
+        </footer>
+    </div>
+    <script type="text/javascript">
+        new FinisherHeader({
+        "count": 6,
+        "size": {
+            "min": 1304,
+            "max": 1500,
+            "pulse": 0
+        },
+        "speed": {
+            "x": {
+            "min": 0.6,
+            "max": 1.6
+            },
+            "y": {
+            "min": 0.6,
+            "max": 1.6
+            }
+        },
+        "colors": {
+            "background": "#000000",
+            "particles": [
+            "#430073",
+            "#7000b9"
+            ]
+        },
+        "blending": "lighten",
+        "opacity": {
+            "center": 0.6,
+            "edge": 0
+        },
+        "skew": 0,
+        "shapes": [
+            "c"
+        ]
+        });
+        </script>
+    <script>
+        document.querySelectorAll('.navbar ul a').forEach(link => {
+            link.addEventListener('click', function() {
+                document.querySelector('.navbar ul a.active').classList.remove('active');
+                this.classList.add('active');
+            });
+        });
+    </script>
+</body>
+</html>
